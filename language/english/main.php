a1f974e24e3ef1a86c0f58ca9829d4604dbf4194
yosha_01 <yosha_01>
2006-01-30 11:06:06 +0000

    english language files added.

---
+++ language/english/main.php
@@ -0,0 +1,30 @@
+<?php
+/**
+* @package languege
+* @ignore
+*/
+
+define('_MD_TAGMEMO_CREATE', "TagMemo");
+define('_MD_TAGMEMO_TITLE', "Title");
+define('_MD_TAGMEMO_TAGS', "Tag");
+define('_MD_TAGMEMO_DESC', "Description");
+define('_MD_TAGMEMO_CANCELL', "Cancel");
+define('_MD_TAGMEMO_DELETE', "Delete");
+define('_MD_TAGMEMO_SAVE', "Autosave");
+define('_MD_TAGMEMO_DONE', "Save/Close");
+define('_MD_TAGMEMO_PUBLIC', "¸ø³«ÀßÄê");
+define('_MD_TAGMEMO_ON', "Public");
+//define('_MD_TAGMEMO_OFF', "Private");
+define('_MD_TAGMEMO_DATETIME', "Datetime"); // check
+define('_MD_TAGMEMO_EDIT', "Edit");
+define('_MD_TAGMEMO_SEARCH', "Search");
+define('_MD_TAGMEMO_MESSAGE_SAVE', "Saved"); // check
+define('_MD_TAGMEMO_MESSAGE_DELETE', "Deleted"); // check
+define('_MD_TAGMEMO_LIST_E', "List");
+define('_MD_TAGMEMO_EDIT_E', "Edit");
+define('_MD_TAGMEMO_EDIT_W', "Write");
+define('_MD_TAGMEMO_TAGS_E', "Tags");
+define('_MD_TAGMEMO_TAGS_N', "New");
+define('_MD_TAGMEMO_TAGS_P', "Populer");
+define('_MD_TAGMEMO_MESSAGE_DELETE_CONFIRM', "Are you sure to delete the memo?"); // check
+?>
---
+++ language/english/modinfo.php
@@ -0,0 +1,8 @@
+<?php
+/**
+* @package languege
+* @ignore
+*/
+define('_MI_TAGMEMO_NAME', "TagMemo");
+define('_MI_TAGMEMO_DESC', "Easy-to-use memo with tag");
+?>
