77c0e7cd513a33d1f88f70fc55751596baa67e7a
yosha_01 <yosha_01>
2006-08-31 17:07:46 +0000

    contributed by sugar. charset : gbk

---
+++ language/schinese/main.php
@@ -0,0 +1,41 @@
+<?php
+/**
+* @package languege
+* @ignore
+*/
+
+define('_MD_TAGMEMO_CREATE', "标签记事");
+define('_MD_TAGMEMO_TITLE', "标题");
+define('_MD_TAGMEMO_TAGS', "标签");
+define('_MD_TAGMEMO_DESC', "内容");
+define('_MD_TAGMEMO_CANCELL', "取消");
+define('_MD_TAGMEMO_DELETE', "删除");
+define('_MD_TAGMEMO_SAVE', "储存");
+define('_MD_TAGMEMO_DONE', "储存/终了");
+define('_MD_TAGMEMO_PUBLIC', "公开设定");
+define('_MD_TAGMEMO_ON', "公开");
+//define('_MD_TAGMEMO_OFF', "非公开");
+define('_MD_TAGMEMO_DATETIME', "日时");
+define('_MD_TAGMEMO_EDIT', "编集");
+define('_MD_TAGMEMO_MESSAGE_SAVE', "记事已储存。");
+define('_MD_TAGMEMO_MESSAGE_DELETE', "记事已删除");
+define('_MD_TAGMEMO_LIST_E', "列表");
+define('_MD_TAGMEMO_EDIT_E', "编辑");
+define('_MD_TAGMEMO_EDIT_W', "建立");
+define('_MD_TAGMEMO_TAGS_E', "标签");
+define('_MD_TAGMEMO_TAGS_N', "新着");
+define('_MD_TAGMEMO_TAGS_P', "人气");
+define('_MD_TAGMEMO_MESSAGE_DELETE_CONFIRM', "是否确定删除这则记事？");
+define('_MD_TAGMEMO_IMG_LIST', "记事列表");
+define('_MD_TAGMEMO_IMG_EDIT', "新增记事");
+define('_MD_TAGMEMO_IMG_CLOUD', "标签云");
+define('_MD_TAGMEMO_TAGCLICK_DELETE', "点选删除");
+define('_MD_TAGMEMO_PLUGIN_FFSEARCH', "Firefox search plugin");
+define('_MD_TAGMEMO_BOOKMARKLET', "标签记事");
+//define('_MD_TAGMEMO_SEARCH', "检索");
+//define('_MD_TAGMEMO_SEARCH_ALL', "全体");
+//define('_MD_TAGMEMO_SEARCH_SUB', "この中から");
+define('_MD_TAGMEMO_CLOSE2OPEN', "关闭已经开启的书签工具视窗？");
+define('_MD_TAGMEMO_NOTWORKFRAME', "书签工具无法在框架页下使用");
+define('_MD_TAGMEMO_RETRYPLEASE', "书签工具启动失败, 请重新启动。");
+?>
---
+++ language/schinese/modinfo.php
@@ -0,0 +1,31 @@
+<?php
+/**
+* @package languege
+* @ignore
+*/
+define('_MI_TAGMEMO_NAME', "标签记事");
+define('_MI_TAGMEMO_DESC', "简单的记事本。");
+//define('_MI_TAGMEMO_SEARCH', "检索");
+define('_MI_TAGMEMO_POPULAR_TAG_BLOCK',"人气标签");
+define('_MI_TAGMEMO_RECENT_TAG_BLOCK',"新进标签");
+define('_MI_TAGMEMO_SERCH_TAG_BLOCK',"搜寻标签");
+define('_MI_TAGMEMO_TAGCLOUD_BLOCK',"标签云");
+//define('_MI_TAGMEMO_SEARCH_ALL', "全体");
+//define('_MI_TAGMEMO_SEARCH_SUB', "この中から");
+define('_MI_TAGMEMO_USE_FF_PLUGIN', "Firefox search plugin");
+define('_MI_TAGMEMO_PER_PAGE', "每页显示的笔记件数");
+define('_MI_TAGMEMO_USE_AUTOLINK', "内文若有包含标签则产生超连结");
+define('_MI_TAGMEMO_KAKASI_PATH', "取得读音假名以'KAKASI'实行转换");
+define('_MI_TAGMEMO_TAGPOPUP_LIST', "标签连结以跳出网页显示(列表)");
+define('_MI_TAGMEMO_TAGPOPUP_DETAIL', "标签连结以跳出网页显示(详细)");
+define('_MI_TAGMEMO_TAGPOPUP_CLOUD', "标签连结以跳出网页显示(标签云)");
+
+define('_MI_TAGMEMO_ADMENU1','首页');
+define('_MI_TAGMEMO_ADMENU2','区块管理');
+define('_MI_TAGMEMO_UPDATE','更新模组');
+define('_MI_TAGMEMO_GOMODULES','前台首页');
+
+define('_MI_TAGMEMO_LIST', "记事列表");
+define('_MI_TAGMEMO_EDIT', "新增记事");
+define('_MI_TAGMEMO_CLOUD', "标签云");
+?>
